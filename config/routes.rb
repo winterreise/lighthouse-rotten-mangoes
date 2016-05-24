88c8442343d4fd346c8205d748c9d668d0b96fb3
winterreise <puiwing.tam@gmail.com>
2016-05-24 03:51:11 +0000

    Implement step 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 RottenMangoes::Application.routes.draw do
-  get "users/new"
-  get "users/create"
+  get "sessions/new"
+  get "sessions/create"
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
