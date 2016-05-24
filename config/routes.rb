4521fd58130c513ea8b3c8fd01c027ded5eeba82
winterreise <puiwing.tam@gmail.com>
2016-05-24 03:05:44 +0000

    Implement step 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RottenMangoes::Application.routes.draw do
+  resources :movies
+end  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ RottenMangoes::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
@@ -53,4 +55,3 @@ RottenMangoes::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
