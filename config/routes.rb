4a2b1058a677b835146f18f51059de698d17d970
winterreise <puiwing.tam@gmail.com>
2016-05-26 03:02:31 +0000

    Updated to session singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ RottenMangoes::Application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   namespace :admin do
     resources :users
